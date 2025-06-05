74e9c685ce1b760f63f631f5be2de2ee62d1af06
recomrad <vt@krdnet.ru>
2025-06-05 13:16:02 +0300

    Update update_fullblock_ipset.sh

--- update_fullblock_ipset.sh
+++ update_fullblock_ipset.sh
@@ -1,18 +1,46 @@
 #! /bin/sh
 
-fetch https://raw.githubusercontent.com/itdoginfo/allow-domains/main/Russia/inside-dnsmasq-ipset.lst -o /tmp/ipset.lst
-sed -e 's/vpn_domains/IPSET_VPN_FULL/g' /tmp/ipset.lst > /usr/local/etc/dnsmasq.conf.d/50_IPSET_VPN_FULL.conf
+if (fetch https://raw.githubusercontent.com/itdoginfo/allow-domains/main/Russia/inside-dnsmasq-ipset.lst --no-verify-hostname -o /tmp/ipset.lst)
+then 
+    echo success-vpn-full
+    sed -e 's/vpn_domains/IPSET_VPN_FULL/g' /tmp/ipset.lst > /usr/local/etc/dnsmasq.conf.d/50_IPSET_VPN_FULL.conf
+fi
 
-fetch https://raw.githubusercontent.com/itdoginfo/allow-domains/main/Russia/outside-dnsmasq-ipset.lst -o /tmp/ipset.lst
-sed -e 's/vpn_domains/IPSET_VPN_RUONLY/g' /tmp/ipset.lst > /usr/local/etc/dnsmasq.conf.d/60_IPSET_VPN_RUONLY.conf
+if (fetch https://raw.githubusercontent.com/itdoginfo/allow-domains/main/Russia/outside-dnsmasq-ipset.lst --no-verify-hostname -o /tmp/ipset.lst)
+then
+    echo success-vpn-ru
+    sed -e 's/vpn_domains/IPSET_VPN_RUONLY/g' /tmp/ipset.lst > /usr/local/etc/dnsmasq.conf.d/60_IPSET_VPN_RUONLY.conf
+fi
 
-fetch https://raw.githubusercontent.com/recomrad/opnsense-ipsets/main/00_DNS_REBIND.conf -o /usr/local/etc/dnsmasq.conf.d/00_DNS_REBIND.conf
-fetch https://raw.githubusercontent.com/recomrad/opnsense-ipsets/main/20_IPSET_VPN_ESSENTIAL.conf -o /usr/local/etc/dnsmasq.conf.d/20_IPSET_VPN_ESSENTIAL.conf
-fetch https://raw.githubusercontent.com/recomrad/opnsense-ipsets/main/30_IPSET_KINO.conf -o /usr/local/etc/dnsmasq.conf.d/30_IPSET_KINO.conf
-fetch https://raw.githubusercontent.com/recomrad/opnsense-ipsets/main/40_IPSET_MICROSOFT.conf -o /usr/local/etc/dnsmasq.conf.d/40_IPSET_MICROSOFT.conf
-fetch https://raw.githubusercontent.com/recomrad/opnsense-ipsets/main/99_IPSET_SPEEDTEST.conf -o /usr/local/etc/dnsmasq.conf.d/99_IPSET_SPEEDTEST.conf
+if (fetch https://raw.githubusercontent.com/recomrad/opnsense-ipsets/main/20_IPSET_VPN_ESSENTIAL.conf --no-verify-hostname -o /tmp/ipset.lst)
+then
+    echo success-vpn-essential
+    cp /tmp/ipset.lst /usr/local/etc/dnsmasq.conf.d/20_IPSET_VPN_ESSENTIAL.conf
+fi
+
+if (fetch https://raw.githubusercontent.com/recomrad/opnsense-ipsets/main/30_IPSET_KINO.conf --no-verify-hostname -o /tmp/ipset.lst)
+then
+    echo success-vpn-kino
+    cp /tmp/ipset.lst /usr/local/etc/dnsmasq.conf.d/30_IPSET_KINO.conf
+fi
+
+if (fetch https://raw.githubusercontent.com/recomrad/opnsense-ipsets/main/40_IPSET_MICROSOFT.conf --no-verify-hostname -o /tmp/ipset.lst)
+then
+    echo success-vpn-microsoft
+    cp /tmp/ipset.lst /usr/local/etc/dnsmasq.conf.d/40_IPSET_MICROSOFT.conf
+fi
+
+if (fetch https://raw.githubusercontent.com/recomrad/opnsense-ipsets/main/99_IPSET_SPEEDTEST.conf --no-verify-hostname -o /tmp/ipset.lst)
+then
+    echo success-vpn-speedtest
+    cp /tmp/ipset.lst /usr/local/etc/dnsmasq.conf.d/99_IPSET_SPEEDTEST.conf
+fi
 
 pluginctl dns
 
-fetch https://raw.githubusercontent.com/recomrad/opnsense-ipsets/main/update_fullblock_ipset.sh -o /usr/bin/update_fullblock_ipset.sh
-chmod +x /usr/bin/update_fullblock_ipset.sh
+if (https://raw.githubusercontent.com/recomrad/opnsense-ipsets/main/update_fullblock_ipset.sh --no-verify-hostname -o /tmp/script.sh)
+then
+    echo success-udpate-script
+    cp /tmp/script.sh /usr/bin/update_fullblock_ipset.sh
+    chmod +x /usr/bin/update_fullblock_ipset.sh
+fi

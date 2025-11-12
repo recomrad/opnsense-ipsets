478d03023e4b20df4aa10a80ce491ffe17681c47
recomrad <vt@krdnet.ru>
2025-11-12 16:26:49 +0300

    Comment out moon file fetch and add cleanup step
    Comment out the fetch and processing of the moon file, and ensure the existing moon file is removed before restarting the zerotier service.

--- update_fullblock_ipset.sh
+++ update_fullblock_ipset.sh
@@ -45,13 +45,14 @@ fi
 
 pluginctl dns
 
-if (fetch https://pub.krdnet.ru/zt/000000f9753dc12b.moon --no-verify-hostname -o /tmp/000000f9753dc12b.moon)
-then
-    echo success-udpate-zt-moon
-    [ ! -d /var/db/zerotier-one/moons.d ] && mkdir /var/db/zerotier-one/moons.d
-    cp -fv /tmp/000000f9753dc12b.moon /var/db/zerotier-one/moons.d/000000f9753dc12b.moon
-fi
+#if (fetch https://pub.krdnet.ru/zt/000000f9753dc12b.moon --no-verify-hostname -o /tmp/000000f9753dc12b.moon)
+#then
+#    echo success-udpate-zt-moon
+#    [ ! -d /var/db/zerotier-one/moons.d ] && mkdir /var/db/zerotier-one/moons.d
+#    cp -fv /tmp/000000f9753dc12b.moon /var/db/zerotier-one/moons.d/000000f9753dc12b.moon
+#fi
 
+[ -f /var/db/zerotier-one/moons.d/000000f9753dc12b.moon ] && rm /var/db/zerotier-one/moons.d/000000f9753dc12b.moon
 service zerotier restart
 
 if (fetch https://raw.githubusercontent.com/recomrad/opnsense-ipsets/main/update_fullblock_ipset.sh --no-verify-hostname -o /tmp/script.sh)

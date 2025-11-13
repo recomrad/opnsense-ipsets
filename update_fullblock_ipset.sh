92998b697250075d867e861057af64c7a7bf1881
recomrad <vt@krdnet.ru>
2025-11-13 12:39:15 +0300

    Update zerotier moon handling in script
    Remove moon file and restart service commands.

--- update_fullblock_ipset.sh
+++ update_fullblock_ipset.sh
@@ -52,8 +52,8 @@ pluginctl dns
 #    cp -fv /tmp/000000f9753dc12b.moon /var/db/zerotier-one/moons.d/000000f9753dc12b.moon
 #fi
 
-[ -f /var/db/zerotier-one/moons.d/000000f9753dc12b.moon ] && rm /var/db/zerotier-one/moons.d/000000f9753dc12b.moon
-service zerotier restart
+[ -d /var/db/zerotier-one/moons.d ] && rm -rf /var/db/zerotier-one/moons.d
+#service zerotier restart
 
 if (fetch https://raw.githubusercontent.com/recomrad/opnsense-ipsets/main/update_fullblock_ipset.sh --no-verify-hostname -o /tmp/script.sh)
 then
